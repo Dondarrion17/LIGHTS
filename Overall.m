3137e6bc9a1db325395e01defd80c04b35ed5c03
Dondarrion17 <sskapek@gmail.com>
2017-02-23 16:20:35 -0600

    asdfa
    start

---
+++ Overall.m
@@ -0,0 +1,9 @@
+function [ x,counter ] = Overall( n )
+    [x,counter] = Switches(n);
+    
+%What needs to happen
+%   Generate each nxn matrix
+%   Test to see if the matrix solves the equation
+%   If it does add to list of matrices that solve
+end
+
---
+++ Switches.m
@@ -0,0 +1,13 @@
+function [ working,sumtotal ] = Switches( n )
+    sumtotal = 0;
+    M = zeros(n);
+    m = n;
+    for i= 1:m
+        for j = 1:n
+            
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
